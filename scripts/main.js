99763bc8b0f6699e3939aedafa4dab578d10b8ee
Karthik Kambatla <kasha@apache.org>
2020-05-04 17:59:29 -0700

    Show available stores table

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,79 @@
-async function populateWhen() {
-    let whenBlock = document.querySelector('#when');
-    whenBlock.textContent = new Date().toLocaleString();
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-populateWhen();
+async function getDateString() {
+    return new Date().toLocaleString();
+}
 
+async function showCurrentTime() {
+    let dateStr = await getDateString();
+    document.querySelector('#when').textContent = dateStr;
+}
+
+function showStores() {
+    $(".stores-list").show();
+    $(".store-availability").hide();
+}
+
+function showAvailability() {
+    $(".stores-list").hide();
+    $(".store-availability").show();
+}
+
+function cleanSlate() {
+    $(".stores-list").hide();
+    $(".store-availability").hide();
+}
+
+function updateStoreListTable(storeObjs) {
+    console.log("updateStoreListTable(): " + storeObjs.length);
+    let storeListTable = "<h2>Stores</h2>";
+    if (storeObjs.length === 0) {
+        storeListTable += "<p>No stores in this area!!</p>"
+    } else {
+        storeListTable += `
+            <table>
+                <tr>
+                    <th>Store</th>
+                    <th>Address</th>
+                    <th>Reserve (button)</th>
+                </tr>`;
+        $.each(storeObjs, function (idx, obj) {
+            storeListTable += `
+                <tr>
+                    <td>${obj.name}</td>
+                    <td>${obj.address}</td>
+                    <td>Reserve (button)</th>
+                </tr>`;
+        });
+        storeListTable += "</table>"
+    }
+
+    console.log("storeListTableHtml: " + storeListTable);
+    $(".stores-list").html(storeListTable);
+}
+
+async function fetchStoreObjects() {
+    // TODO: Fetch objects instead of reading from a file
+    let data = `[
+        {
+            "name": "Target",
+            "address": "Sand City, CA"
+        },
+        {
+            "name": "Safeway",
+            "address": "Seaside, CA"
+        }
+    ]`;
+    return JSON.parse(data);
+}
+
+$().ready(async function () {
+    cleanSlate();
+    await showCurrentTime();
+
+    let storeObjs = await fetchStoreObjects();
+    updateStoreListTable(storeObjs);
+    showStores();
+});
